13d4d9744c181d4aade84796189186ddb59cdad7
romaprd <deyvyalex2006@gmail.com>
2024-09-13 17:15:28 -0300

    fibonacci.c

--- fibonatti.c
+++ fibonatti.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
+
 // Função recursiva para calcular o n-ésimo número de Fibonacci
 int fibonacci(int n) {
+    // Caso base: se n é 0 ou 1, retorne n diretamente
     if (n <= 1) {
         return n;
+    } else {
+        // Caso geral: calcula o n-ésimo número de Fibonacci chamando a função recursivamente
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
- // Caso base: se n for 0 ou 1, retorna n (porque Fibonacci(0) = 0 e Fibonacci(1) = 1)
-    int a = 0, b = 1, c;
-    for (int i = 2; i <= n; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-
-    return b;
 }
 
 int main() {
     int n;
+
+    // Solicita ao usuário a posição desejada na sequência de Fibonacci
     printf("Digite o valor de n: ");
     scanf("%d", &n);
 
+    // Verifica se o valor de n é um número inteiro não negativo
     if (n < 0) {
-        printf("O valor de n deve ser não-negativo.\n");
+        printf("O valor de n deve ser um número inteiro não negativo.\n");
     } else {
+        // Chama a função fibonacci e exibe o resultado
         printf("Fibonacci(%d) = %d\n", n, fibonacci(n));
     }
 
